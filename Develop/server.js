c780860ea4f14154fc3b2416625af0f82b84454d
anton <antonosland789@gmail.com>
2024-05-19 18:39:26 -0500

    more routes added, bug with readFile fs function

--- Develop/server.js
+++ Develop/server.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 
+const {readFile, writeFile} = require('fs');
+
+const Uid = require('short-unique-id');
+
 const path = require('path');
 
 const noteDb = require('./db/db.json');
+
+const exp = require('constants');
 
 const PORT = 3001;
 
@@ -13,16 +19,49 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
+app.use(express.json());
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 app.get('/api/notes', (req, res) => {
+    res.json(noteDb)
+});
+
+app.post('/api/notes', (req, res) => {
+    res.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            const newNote = req.body;
+
+            noteDb.push(newNote);
+
+            const stringifyNoteDb = JSON.stringify(noteDb);
+            writeFile('./db/db.json', stringifyNoteDb, (writeErr) => {
+                if (writeErr) {
+                    console.error(writeErr);
+                }
+                else {
+                    console.log('updated db/db.json');
+                };
+            });
+        };
+    });
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
 
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    res.console.log(noteDb);
+});
 
 
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
 
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
