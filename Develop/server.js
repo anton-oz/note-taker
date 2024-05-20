93bffc9e4a724e80686d4c4327d3e5ab10713d93
anton <antonosland789@gmail.com>
2024-05-19 20:12:44 -0500

    notes have numeric id that increments by 1

--- Develop/server.js
+++ Develop/server.js
@@ -23,21 +23,19 @@ app.use(express.json());
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
-app.get('/api/notes', (req, res) => {
-    res.json(noteDb)
-});
+app.get('/api/notes', (req, res) => res.json(noteDb));
 
 app.post('/api/notes', (req, res) => {
-    res.readFile('./db/db.json', 'utf8', (err, data) => {
+    readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
         }
         else {
             const newNote = req.body;
-
+            console.log('\n', newNote, '\n');
+            newNote.id = noteDb.length === 0 ? 1 : noteDb[noteDb.length - 1].id + 1;
             noteDb.push(newNote);
-
-            const stringifyNoteDb = JSON.stringify(noteDb);
+            const stringifyNoteDb = JSON.stringify(noteDb, '', 4);
             writeFile('./db/db.json', stringifyNoteDb, (writeErr) => {
                 if (writeErr) {
                     console.error(writeErr);
@@ -53,10 +51,11 @@ app.post('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
 
-})
+})  
 
 app.delete('/api/notes/:id', (req, res) => {
-    res.console.log(noteDb);
+    const noteId = req.params.id;
+    console.log(noteDb);
 });
 
 
